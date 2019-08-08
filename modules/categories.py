44f7a0b6c37b5012ea8b78d8436cabb1f8bfca8e
plarabee <p.d.larabee@gmail.com>
2019-08-08 19:27:53 -0400

    stored categories in a dictionary, simplified url generation

--- modules/categories.py
+++ modules/categories.py
@@ -6,82 +6,24 @@ import sys
 
 root = 'http://dotshare.it/category/'
 
+subdomain = {  'irssi': 'chat/irssi/', 'weechat': 'chat/weechat/', 'emacs-colors': 'emacs/colors/', 
+               'emacs-config': 'emacs/config/', 'mc': 'fms/mc/', 'ranger': 'fms/ranger/',
+               'conky': 'info/conky/', 'dzen2': 'info/dzen2/', 'misc': 'misc/misc/',
+               'ncmpcpp': 'mpd/ncmpcpp', 'bmpanel': 'panels/bmpanel/', 'lxpanel': 'panels/lxpanel/',
+               'pypanel': 'panels/pypanel/', 'tint2': 'panels/tint2/', 'bash': 'shells/bash/',
+               'tcsh': 'shells/tcsh/', 'zsh': 'shells/zsh/', 'terms-colors': 'terms/colors/',
+               'screen': 'terms/screen/', 'tmux': 'terms/tmux/', 'vim-colors': 'vim/colors/',
+               'vim-rc': 'vim/rc/', 'awesome': 'wms/awesome/', 'bspwm': 'wms/bspwm/', 'dwm': 'wms/dwm/',
+               'fluxbox': 'wms/fluxbox/', 'fvwm': 'wms/fvwm/', 'herbstluft': 'wms/herbstluft/',
+               'i3': 'wms/i3/', 'monster': 'wms/monster/', 'openbox': 'wms/openbox/',
+               'ratpoison': 'wms/ratpoison/', 'spectrwm': 'wms/spectrwm/', 'stumpwm': 'wms/stumpwm/',
+               'wmfs': 'wms/wmfs/', 'xmonad': 'wms/xmonad/' }
+
 def get_category_url(category):
 
-    if category.lower() == 'irssi':
-        sub = 'chat/irssi/'
-    elif category.lower() == 'weechat':
-        sub = 'chat/weechat/'
-    elif category.lower() == 'emacs-colors':
-        sub = 'emacs/colors/'
-    elif category.lower() == 'emacs-config':
-        sub = 'emacs/config/'
-    elif category.lower() == 'mc':
-        sub = 'fms/mc/'
-    elif category.lower() == 'ranger':
-        sub = 'fms/ranger/'
-    elif category.lower() == 'conky':
-        sub = 'info/conky/'
-    elif category.lower() == 'dzen2':
-        sub = 'info/dzen2/'
-    elif category.lower() == 'misc':
-        sub = 'misc/misc/'
-    elif category.lower() == 'ncmpcpp':
-        sub = 'mpd/ncmpcpp/'
-    elif category.lower() == 'bmpanel':
-        sub = 'panels/bmpanel/'
-    elif category.lower() == 'lxpanel':
-        sub = 'panels/lxpanel/'
-    elif category.lower() == 'pypanel':
-        sub = 'panels/pypanel/'
-    elif category.lower() == 'tint2':
-        sub = 'panels/tint2/'
-    elif category.lower() == 'bash':
-        sub = 'shells/bash/'
-    elif category.lower() == 'tcsh':
-        sub = 'shells/tcsh/'
-    elif category.lower() == 'zsh':
-        sub = 'shells/zsh/'
-    elif category.lower() == 'terms-colors':
-        sub = 'terms/colors/'
-    elif category.lower() == 'screen':
-        sub = 'terms/screen/'
-    elif category.lower() == 'tmux':
-        sub = 'terms/tmux/'
-    elif category.lower() == 'vim-colors':
-        sub = 'vim/colors/'
-    elif category.lower() == 'vim-rc':
-        sub = 'vim/rc/'
-    elif category.lower() == 'awesome':
-        sub = 'wms/awesome/'
-    elif category.lower() == 'bspwm':
-        sub = 'wms/bspwm/'
-    elif category.lower() == 'dwm':
-        sub = 'wms/dwm/'
-    elif category.lower() == 'fluxbox':
-        sub = 'wms/fluxbox/'
-    elif category.lower() == 'fvwm':
-        sub = 'wms/fvwm/'
-    elif category.lower() == 'herbstluft':
-        sub = 'wms/herbstluft/'
-    elif category.lower() == 'i3':
-        sub = 'wms/i3/'
-    elif category.lower() == 'monster':
-        sub = 'wms/monster/'
-    elif category.lower() == 'openbox':
-        sub = 'wms/openbox/'
-    elif category.lower() == 'ratpoison':
-        sub = 'wms/ratpoison/'
-    elif category.lower() == 'spectrwm':
-        sub = 'wms/spectrwm/'
-    elif category.lower() == 'stumpwm':
-        sub = 'wms/stumpwm/'
-    elif category.lower() == 'wmfs':
-        sub = 'wms/wmfs/'
-    elif category.lower() == 'xmonad':
-        sub = 'wms/xmonad/'
+    if category in subdomain.keys():
+        return root + subdomain[category]
+    
     else:
-        print('ERROR: Category %s not found.' % category)
+        print('Category not found!')
         sys.exit()
-
-    return root + sub
